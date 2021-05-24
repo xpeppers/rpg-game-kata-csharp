acb82095bb4b392e5a49a30e0c665414d388df8e
Pietro Di Bello <pierodibello@gmail.com>
2021-05-24 12:20:02 +0200

    first commit

---
+++ RpgGameKata.Tests/ExampleTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace RpgGameKata.Tests
+{
+    public class ExampleTest
+    {
+        [Fact]
+        public void A_Simple_Test()
+        {
+            Assert.True(false); // Fix me first!
+        }
+    }
+}
