b9de84e4566f73fa389038be0dacb919b2c998e3
Gigivi <luigi.calabrese@aptos.com>
2021-05-26 11:48:52 +0200

    Refactoring: moved Character and renamed CharacterTest classes

--- RpgGameKata.Tests/CharacterTest.cs
+++ RpgGameKata.Tests/CharacterTest.cs
@@ -1,9 +1,8 @@
-using System;
 using Xunit;
 
 namespace RpgGameKata.Tests
 {
-	public class ExampleTest
+    public class CharacterTest
 	{
 		[Fact]
 		public void Damage_To_Character_Decrease_His_Health()
@@ -27,20 +26,5 @@ namespace RpgGameKata.Tests
 			Assert.Equal(0, character2.Health);
             Assert.False(character2.IsAlive);
         }
-	}
-
-	public class Character
-	{
-		public int Health { get; set; } = 1000;
-		public bool IsAlive { get
-			{
-				return Health > 0;
-			}
-		}
-
-        internal void Attack(Character character2, int damage)
-		{
-			character2.Health -= Math.Min(character2.Health, damage);
-		}
 	}
 }
---
+++ RpgGameKata/Character.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RpgGameKata
+{
+    public class Character
+	{
+		public int Health { get; set; } = 1000;
+		public bool IsAlive { get
+			{
+				return Health > 0;
+			}
+		}
+
+        public void Attack(Character character2, int damage)
+		{
+			character2.Health -= Math.Min(character2.Health, damage);
+		}
+	}
+}
