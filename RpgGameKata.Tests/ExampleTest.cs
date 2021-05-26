90fcdffe6b831c366d93b3cd1dd5c5f27fc4e83a
Andrea Carlisi <acarlisi@Aptos.net>
2021-05-26 11:26:00 +0200

    Developed when Health becomes 0 character dies

--- RpgGameKata.Tests/ExampleTest.cs
+++ RpgGameKata.Tests/ExampleTest.cs
@@ -25,15 +25,20 @@ namespace RpgGameKata.Tests
 			character1.Attack(character2, 1001);
 
 			Assert.Equal(0, character2.Health);
-			//Assert.False(character2.IsAlive);
-		}
+            Assert.False(character2.IsAlive);
+        }
 	}
 
 	public class Character
 	{
 		public int Health { get; set; } = 1000;
+		public bool IsAlive { get
+			{
+				return Health > 0;
+			}
+		}
 
-		internal void Attack(Character character2, int damage)
+        internal void Attack(Character character2, int damage)
 		{
 			character2.Health -= Math.Min(character2.Health, damage);
 		}
